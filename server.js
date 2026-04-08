ac410138c8095076254e0f6f53f3c3a3c720befc
Brammanayagan S <sakthibrammanayagan@gmail.com>
2026-04-09 01:18:44 +0530

    server setup

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
